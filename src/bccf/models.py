0c098e15eb575f908d35a2b6760efdf36781b917
Karl Castillo <khcastillo@hotmail.com>
2013-10-15 13:45:46 -0700

    added stars

--- src/bccf/models.py
+++ src/bccf/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class Topic(models.Model):
     name = models.CharField(max_length=255)
     star_blog_id = models.IntegerField(null=True, blank=True)
