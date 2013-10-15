d91796ae313ba3f3228a30ac5f4a815dad78165b
Karl Castillo <khcastillo@hotmail.com>
2013-10-15 12:27:11 -0700

    Removed rating model - will use mezzanine built in rating

--- src/bccf/models.py
+++ src/bccf/models.py
@@ -14,13 +14,3 @@ class TopicLink(models.Model):
     topic = models.ForeignKey(Topic)
     model_name = models.CharField(max_length=255)
     entity_id = models.IntegerField()
-
-
-RATINGS = [(str(i), i) for i in range(1, 6)]
-
-class Rating(models.Model):
-    model_name = models.CharField(max_length=255)
-    entity_id = models.IntegerField()
-    rated_by = models.ForeignKey(User)
-    rated_on = models.DateTimeField(auto_now_add=True, blank=True)
-    rating = models.IntegerField(choices=RATINGS)
