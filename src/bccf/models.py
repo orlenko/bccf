7967ebee4e3b5ea6395347daee25e1dfa27e6dcd
Vlad Orlenko <Vlad Orlenko>
2013-10-22 12:22:26 -0700

    Simple shop with interrelated products

--- src/bccf/models.py
+++ src/bccf/models.py
@@ -8,8 +8,18 @@ class Topic(models.Model):
     star_survey_id = models.IntegerField(null=True, blank=True)
     star_forum_post_id = models.IntegerField(null=True, blank=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class TopicLink(models.Model):
     topic = models.ForeignKey(Topic)
     model_name = models.CharField(max_length=255)
     entity_id = models.IntegerField()
+
+    @property
+    def target(self):
+        return globals()[self.model_name].objects.get(pk=self.entity_id)
+
+    def __unicode__(self):
+        return '%s - %s' % (self.topic.name, self.target)
