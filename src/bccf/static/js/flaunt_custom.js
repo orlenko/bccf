b0b8c944cb3471f49f7c38753925c617a2cd6ae8
Karl Castillo <khcastillo@hotmail.com>
2013-11-22 15:57:01 -0800

    front page menu and marquee

--- src/bccf/static/js/flaunt_custom.js
+++ src/bccf/static/js/flaunt_custom.js
@@ -1,4 +1,4 @@
-(function($, window) {
+(function($) {
     var Flaunt = {
         init: function(options, elem) {
             var base = this;
@@ -20,27 +20,45 @@
             base.$elem.append(mobileContainer);
             base.$elem.children('.nav-list').children('.nav-item').has('ul').prepend('<span class="nav-click"><i class="nav-arrow"></i></span>');                        
             mobileContainer.on('click', function() {
-                base.$elem.children('.nav-list').slideToggle(200);        
+                base.$elem.children('.nav-list:not(li)').slideToggle(200);        
+            });
+            base.$elem.children('.nav-list').on('mouseenter', '.nav-item', function(e) {
+                if(!mobileContainer.is(':visible')) {
+                    $(this).children('.nav-submenu:not(li)').slideToggle(200);
+                    $(this).children('.nav-arrow').toggleClass('nav-rotate');
+                }          
+            });
+            base.$elem.children('.nav-list').on('mouseleave', '.nav-item', function(e) {
+                if(!mobileContainer.is(':visible')) {
+                    $(this).children('.nav-submenu:not(li)').slideToggle(200);
+                    $(this).children('.nav-arrow').toggleClass('nav-rotate');
+                }          
             });
             base.$elem.children('.nav-list').on('click', '.nav-click', function() {
-                $(this).siblings('.nav-submenu').toggle();
+                $(this).siblings('.nav-submenu:not(li)').slideToggle(200);
                 $(this).children('.nav-arrow').toggleClass('nav-rotate');
             });
             $(window).resize(function() {
+                if(!mobileContainer.is(':visible')) {
+                    base.$elem.find('.nav-submenu').hide();    
+                }
                 if(!mobileContainer.is(':visible') && !base.$elem.children('.nav-list').is(':visible')) {
-                    console.log('resize');
-                    base.$elem.children('.nav-list').show(); 
-                } else if(mobileContainer.is(':visible') && base.$elem.children('.nav-list').is(':visible')) {
-                    base.$elem.children('.nav-list').hide();     
+                    base.$elem.children('.nav-list').show();
                 }
             });
         },
         prepContainer: function() {
             var base = this; 
             base.$elem.children('ul').addClass('nav-list');
-            base.$elem.children('.nav-list').children('li').addClass('nav-item');
-            base.$elem.children('.nav-list').children('li').children('ul').addClass('nav-submenu');
-            base.$elem.children('.nav-list').children('li').children('ul').children('li').addClass('nav-submenu-item');    
+            base.$elem.children('.nav-list')
+                .children('li').addClass('nav-item');
+            base.$elem.children('.nav-list')
+                .children('li')
+                    .children('ul').addClass('nav-submenu');
+            base.$elem.children('.nav-list')
+                .children('li').children('ul')
+                    .children('li')
+                        .addClass('nav-submenu-item');    
         },
     };
     $.fn.Flaunt = function(options) {
@@ -53,4 +71,4 @@
     $.fn.Flaunt.options = {
         mobileContainer: null   // Id of the DIV that will contain the mobile button
     };
-})(jQuery, window);
+})(jQuery);
