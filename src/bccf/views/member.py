c8ba906408bd446db0e914dcbe7c55352aa5c72b
Karl Castillo <khcastillo@hotmail.com>
2014-02-25 14:25:25 -0800

    fixed request program

--- src/bccf/views/member.py
+++ src/bccf/views/member.py
@@ -13,7 +13,7 @@ from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
 
 from bccf.util.memberutil import get_upgrades, require_any_membership
-from bccf.forms import AddUserForm, AddExistingUserForm, DelMember, AddUsersForm
+from bccf.forms import AddUserForm, AddExistingUserForm, DelMember, AddUsersForm, ProgramRequestForm
 from bccf.models import ProgramRequest
 
 log = logging.getLogger(__name__)
