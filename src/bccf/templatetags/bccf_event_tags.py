a112f4353e430b3deb5c4cde3bdd4ce7245b55a8
Karl Castillo <khcastillo@hotmail.com>
2014-04-03 12:18:56 -0700

    membership directory

--- src/bccf/templatetags/bccf_event_tags.py
+++ src/bccf/templatetags/bccf_event_tags.py
@@ -5,7 +5,7 @@ from mezzanine import template
 
 from cartridge.shop.forms import AddProductForm
 
-from bccf.models import BCCFChildPage, EventRegistration
+from bccf.models import BCCFChildPage, Event, EventRegistration
 
 import logging
 import re
@@ -30,4 +30,10 @@ def bccf_subscribe_for(context, obj):
 def attendees_for(context, event):
     context['attendees'] = EventRegistration.objects.filter(event=event)
     context['event_obj'] = event.get_content_model()
+    return context
+    
+@register.inclusion_tag("generic/includes/user_event.html", takes_context=True)
+def events_of(context, user):
+    if user.is_level_C:
+        context['event_objs'] = Event.objects.filter(provider=user).order_by('-date_start')
     return context
