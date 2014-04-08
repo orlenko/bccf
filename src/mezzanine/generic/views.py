15e1abef609eea6f8938392339a41c08f3cc51c5
Karl Castillo <khcastillo@hotmail.com>
2014-04-08 15:23:33 -0700

    comment

--- src/mezzanine/generic/views.py
+++ src/mezzanine/generic/views.py
@@ -107,9 +107,6 @@ def comment(request, template="generic/comments.html"):
         return response
     elif request.is_ajax() and form.errors:
         return HttpResponse(dumps({"errors": form.errors}))
-    else:
-        error(request, "%s" % form.errors.items)
-        error(request, "ERROR")
     # Show errors with stand-alone comment form.
     context = {"obj": obj, "posted_comment_form": form}
     response = render(request, template, context)
