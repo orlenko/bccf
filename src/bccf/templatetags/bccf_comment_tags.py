be61fa6f5bdf16313b2d32240a794405a00468c7
Karl Castillo <khcastillo@hotmail.com>
2014-04-09 11:48:01 -0700

    comment

--- src/bccf/templatetags/bccf_comment_tags.py
+++ src/bccf/templatetags/bccf_comment_tags.py
@@ -10,6 +10,7 @@ register = template.Library()
 @register.inclusion_tag("generic/includes/comment/comment.html", takes_context=True)
 def comment_for(context, obj):
     user = User.objects.get(email=obj.email)
+    context['comment_obj'] = obj
     context['comment_user'] = user
     context['comment_profile'] = user.profile
     return context
