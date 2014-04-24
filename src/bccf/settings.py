e4bf333c1abbcdf370da8ab2864ed6ee5f5b34e8
Karl Castillo <khcastillo@hotmail.com>
2014-04-24 14:03:37 -0700

    CKEditor

--- src/bccf/settings.py
+++ src/bccf/settings.py
@@ -600,20 +600,20 @@ CKEDITOR_CONFIGS = {
         'height': 300,
         'allowedContent': True,
     },
-    'basic': {
-        #'toolbar': 'Basic',
+    'basic': { # not for superusers
+        'toolbar': 'Basic',
         'toolbarGroups': [
-           { 'name': 'clipboard',   'groups': [ 'clipboard', 'undo' ] },
-           { 'name': 'editing',     'groups': [ 'find', 'selection', 'spellchecker' ] },
-           { 'name': 'links' },
+           # { 'name': 'clipboard',   'groups': [ 'clipboard', 'undo' ] },
+           # { 'name': 'editing',     'groups': [ 'find', 'selection', 'spellchecker' ] },
+            { 'name': 'links' },
            # { 'name': 'insert' },
            # { 'name': 'forms' },
            # { 'name': 'tools' },
            # { 'name': 'document',       'groups': [ 'mode', 'document', 'doctools' ] },
            # { 'name': 'others' },
            # '/',
-           { 'name': 'basicstyles', 'groups': [ 'basicstyles', 'cleanup' ] },
-           { 'name': 'paragraph',   'groups': [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+            { 'name': 'basicstyles', 'groups': [ 'basicstyles', 'cleanup' ] },
+           # { 'name': 'paragraph',   'groups': [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
            # { 'name': 'styles' },
            # { 'name': 'colors' },
            # { 'name': 'about' }
