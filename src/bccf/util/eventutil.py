e744e0110324839ed24186312f3c71174e6e566d
Vlad Orlenko <vlad@bjola.ca>
2014-07-04 04:07:42 -0700

    Handle empty cart on checkout

--- src/bccf/util/eventutil.py
+++ src/bccf/util/eventutil.py
@@ -17,11 +17,13 @@ def show_bill(request):
     """
     Checks whether or not an event is in the cart and returns false if the
     start date of the event is less than 2 weeks from now.
-    
+
     False means that paypal is the only good method of payment.
     True means the bill method of payment is fine.
     """
     cart = Cart.objects.from_request(request)
+    if not hasattr(cart, 'items'):
+        return False
     for order_item in cart.items.all():
         if order_item.sku.startswith('EVENT-'):
             sku_parts = order_item.sku.split('EVENT-')
@@ -32,7 +34,7 @@ def show_bill(request):
         if order_item.sku.startswith('ORG') or order_item.sku.startswith('PRO'):
             return False
     return True
-    
+
 def handle_event(request, user, order):
     """
     Check if the order has events in it. Process the even and register
@@ -44,13 +46,13 @@ def handle_event(request, user, order):
             sku_parts = order_item.sku.split('EVENT-')
             event = Event.objects.get(id=int(sku_parts[1]))
             paid = False
-            
+
             # If payment is paypal everything is good!
             if order.payment_method == 'paypal':
                 order.status = 2
                 order.save()
                 paid = True
-                
+
                 # Adds a the payment to the total payment for the professional after a cut
                 if not event.provider.is_superuser:
                     cut = event.price * Decimal(Settings.get_setting('BCCF_PERCENTAGE_CUT'))
@@ -61,17 +63,17 @@ def handle_event(request, user, order):
                     else:
                         profile.payment += price
                     profile.save()
-                    
+
             if user.profile.is_organization:
                 user_ids = request.session.get('register_selected_members', [])
                 event_id = request.session.get('register_selected_event', None)
-                
+
                 if not event_id:
                     log.debug('event ID is None')
                     return
                 if event_id != event:
-                    continue                
-                    
+                    continue
+
                 event = Event.objects.get(id=event_id)
                 members = []
                 for user_id in user_ids:
@@ -80,16 +82,16 @@ def handle_event(request, user, order):
                     EventRegistration.objects.create(user=user, event=event,
                                             event_order=order, paid=paid)
                     send_reminder("Event Registration Complete.", u, context={'event':event})
-                    
+
                 send_reminder("Event Registration Complete.", request.user, context={'event': event, 'members': members})
-                                            
+
                 del request.session['register_selected_members']
                 del request.session['register_selected_event']
-                                    
-            else:        
-                EventRegistration.objects.create(user=user, event=event, 
+
+            else:
+                EventRegistration.objects.create(user=user, event=event,
                                          event_order=order, paid=paid)
-                
+
                 # Send confirmation
                 send_reminder("Event Registration Complete.", request.user, context={'event':event})
 
